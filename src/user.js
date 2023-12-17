4de057208d0c939b14716ecf4f869755fa14fd21
Shivansh Singh <singhshivansh12may@gmail.com>
2023-12-17 12:31:52 +0530

    Added autocomplete

--- src/user.js
+++ src/user.js
@@ -1,12 +1,17 @@
 import React,{useEffect,useState} from 'react';
 import axios from 'axios';
+import MenuIcon from '@mui/icons-material/Menu';
+import IconButton from '@mui/material/IconButton';
+import Drawer from '@mui/material/Drawer';
 import Stack from '@mui/material/Stack';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 import { useParams } from 'react-router-dom';
 import Grid from '@mui/material/Grid';
 import { styled } from '@mui/system';
 
 const LeftBox = styled(Grid)(({ theme }) => ({
-  backgroundColor: '#f0f0f0',
+  backgroundColor: '#ffffff',
   height: '100vh',
   borderRight: `1px solid ${theme.palette.divider}`,
   [theme.breakpoints.down('sm')]: {
@@ -22,12 +27,30 @@ const RightBox = styled(Grid)(({ theme }) => ({
   },
 }));
 
+const DrawerContent = () => {
+    // Add your drawer content here
+    return (
+      <div>
+        {/* Drawer content */}
+      </div>
+    );
+  };
+  
 const UserComponent = () => {
     const { id } = useParams();
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [skills, setSkills] = useState([]);
     const [college, setCollege] = useState("");
+    const [drawerOpen, setDrawerOpen] = useState(false);
+
+    const handleDrawerOpen = () => {
+      setDrawerOpen(true);
+    };
+  
+    const handleDrawerClose = () => {
+      setDrawerOpen(false);
+    };
     useEffect(() => {
         const apiurl = `http://localhost:3005/api/v1/getuser/${id}`;
         async function getdata(){
@@ -40,10 +63,40 @@ const UserComponent = () => {
         }
         getdata();
       }, [id]);
+      
   return (
     <Grid container>
-     <LeftBox item xs={12} md={6}>
-       
+      <LeftBox item xs={12} md={6}>
+        {/* Row Stack */}
+        <Stack direction="row" spacing={2}>
+          {/* Drawer Icon */}
+          <IconButton onClick={handleDrawerOpen}>
+            <MenuIcon />
+          </IconButton>
+
+          
+     
+            <Autocomplete
+                options={['Option 1', 'Option 2', 'Option 3']}
+                sx={{ width: '100%',borderRadius: '8px'}}
+                renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label="Search"
+                    variant="outlined"
+                    
+                    style={{ width: '100%', borderRadius: '8px', backgroundColor: '#f0f0f0' }}
+                />
+                )}
+            />
+          </Stack>
+
+        {/* Drawer */}
+        <Drawer anchor="left" open={drawerOpen} onClose={handleDrawerClose}>
+          <DrawerContent />
+        </Drawer>
+
+        {/* Content for the left box */}
       </LeftBox>
 
       
